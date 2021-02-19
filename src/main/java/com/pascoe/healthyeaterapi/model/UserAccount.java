2fb7f84eec22353fbf0a8a7325cc3432f44055ee
jpassgo <jeffpascoe9@gmail.com>
2021-02-19 17:09:57 -0600

    Adding model to represent a users account. Adding mongodb repository for the user account.

---
+++ src/main/java/com/pascoe/healthyeaterapi/model/UserAccount.java
@@ -0,0 +1,59 @@
+package com.pascoe.healthyeaterapi.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+public class UserAccount implements UserDetails {
+
+    @Id
+    private Long id;
+    private String userName;
+    private String password;
+    private String email;
+    private String name;
+    private String surname;
+    private Boolean locked;
+    private Boolean expired;
+    private Boolean enabled;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return expired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
---
+++ src/main/java/com/pascoe/healthyeaterapi/repository/UserAccountRepository.java
@@ -0,0 +1,7 @@
+package com.pascoe.healthyeaterapi.repository;
+
+import com.pascoe.healthyeaterapi.model.UserAccount;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserAccountRepository extends MongoRepository<UserAccount, Long> {
+}
