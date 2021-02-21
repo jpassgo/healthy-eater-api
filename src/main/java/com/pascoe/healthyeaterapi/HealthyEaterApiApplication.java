5db17e9f2be4abbe36f19b4dde0bc1641ae300eb
jpassgo <jeffpascoe9@gmail.com>
2021-02-20 20:30:38 -0600

    Removing untracked files.

--- src/main/java/com/pascoe/healthyeaterapi/HealthyEaterApiApplication.java
+++ src/main/java/com/pascoe/healthyeaterapi/HealthyEaterApiApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeightManagementApplication {
+public class HealthyEaterApiApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(WeightManagementApplication.class, args);
+    SpringApplication.run(HealthyEaterApiApplication.class, args);
   }
 }
