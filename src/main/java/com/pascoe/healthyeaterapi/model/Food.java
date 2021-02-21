83cc0d37cead48949bb76c20361eda11a1430221
jpassgo <jeffpascoe9@gmail.com>
2021-02-21 11:28:36 -0600

    Adding annotations for creating constructors.

--- src/main/java/com/pascoe/healthyeaterapi/model/Food.java
+++ src/main/java/com/pascoe/healthyeaterapi/model/Food.java
@@ -1,5 +1,12 @@
 package com.pascoe.healthyeaterapi.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Food {
 
   private String name;
