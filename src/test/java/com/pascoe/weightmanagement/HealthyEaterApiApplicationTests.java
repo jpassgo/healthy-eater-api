d209a70bb6cafb2f852ec0120fcf4e14fdb21022
jpassgo <jeffpascoe9@gmail.com>
2021-02-23 17:52:50 -0600

    Removing test.

--- src/test/java/com/pascoe/weightmanagement/HealthyEaterApiApplicationTests.java
+++ src/test/java/com/pascoe/weightmanagement/HealthyEaterApiApplicationTests.java
@@ -1,11 +1,6 @@
 package com.pascoe.weightmanagement;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HealthyEaterApiApplicationTests {
-
-  @Test
-  void contextLoads() {}
-}
+class HealthyEaterApiApplicationTests {}
