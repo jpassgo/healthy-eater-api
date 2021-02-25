695d0c2fdd62e6624b3c877093167cbf0c655bbf
jpassgo <jeffpascoe9@gmail.com>
2021-02-24 20:13:41 -0600

    Updating UserAccountRepository to take in a UserAccount.

--- src/main/java/com/pascoe/healthyeaterapi/repository/UserAccountRepository.java
+++ src/main/java/com/pascoe/healthyeaterapi/repository/UserAccountRepository.java
@@ -1,6 +1,6 @@
 package com.pascoe.healthyeaterapi.repository;
 
-import com.pascoe.healthyeaterapi.model.LoginDetails;
+import com.pascoe.healthyeaterapi.model.UserAccount;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserAccountRepository extends CrudRepository<LoginDetails, Integer> {}
+public interface UserAccountRepository extends CrudRepository<UserAccount, Integer> {}
