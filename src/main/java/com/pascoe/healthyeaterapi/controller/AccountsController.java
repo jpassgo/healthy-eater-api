5a203a692650df16278716fd514627437afec0df
jpassgo <jeffpascoe9@gmail.com>
2021-02-25 17:24:09 -0600

    Adding service

--- src/main/java/com/pascoe/healthyeaterapi/controller/AccountsController.java
+++ src/main/java/com/pascoe/healthyeaterapi/controller/AccountsController.java
@@ -2,10 +2,12 @@ package com.pascoe.healthyeaterapi.controller;
 
 import com.pascoe.healthyeaterapi.model.UserAccount;
 import com.pascoe.healthyeaterapi.repository.UserAccountRepository;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Service
 @RestController("/accounts")
 public class AccountsController {
 
