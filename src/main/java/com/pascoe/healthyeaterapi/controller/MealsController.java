c8f69f113c8f9c958d08e44bbaa7dcc163d030cc
jpassgo <jeffpascoe9@gmail.com>
2021-02-26 19:21:18 -0600

    Renaming endpoint.

--- src/main/java/com/pascoe/healthyeaterapi/controller/MealsController.java
+++ src/main/java/com/pascoe/healthyeaterapi/controller/MealsController.java
@@ -1,14 +1,12 @@
 package com.pascoe.healthyeaterapi.controller;
 
 import com.pascoe.healthyeaterapi.model.Meal;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ReportMealsController {
+public class MealsController {
 
   @PostMapping("/reportMeals")
   public void reportMeals(@RequestBody Meal meal) {}
