a31bab14e7311ab5ef8b156c5e10324281251598
jpassgo <jeffpascoe9@gmail.com>
2021-02-28 14:35:24 -0600

    Creating custom toString method.

--- src/main/java/com/pascoe/healthyeaterapi/model/UserAccount.java
+++ src/main/java/com/pascoe/healthyeaterapi/model/UserAccount.java
@@ -18,4 +18,17 @@ public class UserAccount {
   //  Todo: Create Custom emailAddress class for email address validation
   private String emailAddress;
   private LoginDetails loginDetails;
+
+
+  @Override
+  public String toString() {
+    return String.format("firstName: %s \n" +
+            "lastName: %s \n" +
+            "emailAddress: %s \n" +
+            "loginDetails: %s ",
+            this.firstName,
+            this.lastName,
+            this.emailAddress,
+            "Hidden");
+  }
 }
