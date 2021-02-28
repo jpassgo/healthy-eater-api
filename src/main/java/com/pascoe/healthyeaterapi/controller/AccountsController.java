586eeec8affd9e035fe244b694ba7d3b8d1eff90
jpassgo <jeffpascoe9@gmail.com>
2021-02-28 15:35:59 -0600

    Adding a method for retrieving a UserAccount.

--- src/main/java/com/pascoe/healthyeaterapi/controller/AccountsController.java
+++ src/main/java/com/pascoe/healthyeaterapi/controller/AccountsController.java
@@ -5,10 +5,9 @@ import com.pascoe.healthyeaterapi.repository.UserAccountRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
@@ -20,8 +19,18 @@ public class AccountsController {
     @PostMapping
     public ResponseEntity createAccount(@RequestBody UserAccount userAccount) {
         System.out.println(userAccount.toString());
-        userAccountRepository.save(userAccount);
+        UserAccount newUserAccount = userAccountRepository.save(userAccount);
+        System.out.println(newUserAccount.toString());
         return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity retrieveAccount(@PathVariable Integer id) {
+        Optional<UserAccount> optionalUserAccount = userAccountRepository.findById(id);
+
+        return optionalUserAccount.map(
+                userAccount -> new ResponseEntity(userAccount, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
     }
 }
 
