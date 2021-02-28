795ca404ef32ede5d114a399590f0c924d4ff059
jpassgo <jeffpascoe9@gmail.com>
2021-02-28 14:58:18 -0600

    Cleaning up account creation endpoint.

--- src/main/java/com/pascoe/healthyeaterapi/controller/AccountsController.java
+++ src/main/java/com/pascoe/healthyeaterapi/controller/AccountsController.java
@@ -3,18 +3,25 @@ package com.pascoe.healthyeaterapi.controller;
 import com.pascoe.healthyeaterapi.model.UserAccount;
 import com.pascoe.healthyeaterapi.repository.UserAccountRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
+@RequestMapping("/accounts")
 public class AccountsController {
 
-  private UserAccountRepository userAccountRepository;
+    private UserAccountRepository userAccountRepository;
 
-  @PostMapping("/accounts")
-  public void createAccount(@RequestBody UserAccount userAccount) {
-     userAccountRepository.save(userAccount);
-  }
+    @PostMapping
+    public ResponseEntity createAccount(@RequestBody UserAccount userAccount) {
+        System.out.println(userAccount.toString());
+        userAccountRepository.save(userAccount);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
 }
+
