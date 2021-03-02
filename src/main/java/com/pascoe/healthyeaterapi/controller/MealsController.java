4bc508408857a1a8e788bb25af2f328caf9b479f
jpassgo <jeffpascoe9@gmail.com>
2021-03-02 16:38:03 -0600

    Creating a controller for reporting your meals.

--- src/main/java/com/pascoe/healthyeaterapi/controller/MealsController.java
+++ src/main/java/com/pascoe/healthyeaterapi/controller/MealsController.java
@@ -1,13 +1,37 @@
 package com.pascoe.healthyeaterapi.controller;
 
 import com.pascoe.healthyeaterapi.model.Meal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.pascoe.healthyeaterapi.service.MealsService;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
+@RequestMapping("/meals")
 public class MealsController {
 
-  @PostMapping("/reportMeals")
-  public void reportMeals(@RequestBody Meal meal) {}
+  private MealsService mealsService;
+
+  @PostMapping("/{id}")
+  public ResponseEntity reportMeals(@PathVariable Integer id, @RequestBody Meal meal) {
+    try {
+      mealsService.reportMeal(meal);
+      return new ResponseEntity(HttpStatus.CREATED);
+    } catch (IllegalArgumentException e) {
+      return new ResponseEntity(HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity getMeals(@PathVariable Integer id) {
+    try {
+      List<Meal> meals = mealsService.getMeals(id);
+      return new ResponseEntity(meals, HttpStatus.OK);
+    } catch (IllegalArgumentException e) {
+      return new ResponseEntity(HttpStatus.BAD_REQUEST);
+    }
+  }
 }
