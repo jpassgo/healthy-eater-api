0a2becc02df9ebf2c90ca27b4f74c3f76411a665
jpassgo <jeffpascoe9@gmail.com>
2021-03-01 18:28:29 -0600

    Fixing imports and getting class ready to be db entity.

--- src/main/java/com/pascoe/healthyeaterapi/model/Meal.java
+++ src/main/java/com/pascoe/healthyeaterapi/model/Meal.java
@@ -1,15 +1,26 @@
 package com.pascoe.healthyeaterapi.model;
 
-import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+
 @Data
+@Entity
 @NoArgsConstructor
-@AllArgsConstructor
 public class Meal {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+  private Integer userId;
+  @ElementCollection(targetClass = Food.class)
+  @AttributeOverride(name = "name", column = @Column(name = "meal_meal"))
   private List<Food> meal;
   private Integer caloricValue;
+  private Date date;
+
 }
