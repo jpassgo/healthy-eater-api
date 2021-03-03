b0e1c8028a78ac7e7515921677517aca4a90b8b4
jpassgo <jeffpascoe9@gmail.com>
2021-03-03 17:20:45 -0600

    Cleaning up AccountsController.

--- src/main/java/com/pascoe/healthyeaterapi/controller/AccountsController.java
+++ src/main/java/com/pascoe/healthyeaterapi/controller/AccountsController.java
@@ -1,36 +1,34 @@
 package com.pascoe.healthyeaterapi.controller;
 
 import com.pascoe.healthyeaterapi.model.UserAccount;
-import com.pascoe.healthyeaterapi.repository.UserAccountRepository;
+import com.pascoe.healthyeaterapi.service.AccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/accounts")
 public class AccountsController {
 
-    private UserAccountRepository userAccountRepository;
+  private AccountsService accountsService;
 
-    @PostMapping
-    public ResponseEntity createAccount(@RequestBody UserAccount userAccount) {
-        System.out.println(userAccount.toString());
-        UserAccount newUserAccount = userAccountRepository.save(userAccount);
-        System.out.println(newUserAccount.toString());
-        return new ResponseEntity(HttpStatus.CREATED);
+  @PostMapping
+  public ResponseEntity createAccount(@RequestBody UserAccount userAccount) {
+    try {
+        UserAccount account = accountsService.createAccount(userAccount);
+        return new ResponseEntity(account.getId(), HttpStatus.CREATED);
+    } catch (IllegalArgumentException e) {
+      return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
+  }
 
-    @GetMapping("/{id}")
-    public ResponseEntity retrieveAccount(@PathVariable Integer id) {
-        Optional<UserAccount> optionalUserAccount = userAccountRepository.findById(id);
-
-        return optionalUserAccount.map(
-                userAccount -> new ResponseEntity(userAccount, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
-    }
+  @GetMapping("/{id}")
+  public ResponseEntity retrieveAccount(@PathVariable Integer id) {
+    return accountsService
+        .findAccount(id)
+        .map(userAccount -> new ResponseEntity(userAccount, HttpStatus.OK))
+        .orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
+  }
 }
-
