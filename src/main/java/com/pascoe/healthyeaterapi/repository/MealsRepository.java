6c56ddc56cd58976ba843e848829b972234425fe
jpassgo <jeffpascoe9@gmail.com>
2021-03-04 17:44:01 -0600

    Updating repository with a findAllById method.

--- src/main/java/com/pascoe/healthyeaterapi/repository/MealsRepository.java
+++ src/main/java/com/pascoe/healthyeaterapi/repository/MealsRepository.java
@@ -1,6 +1,10 @@
 package com.pascoe.healthyeaterapi.repository;
 
-import com.pascoe.healthyeaterapi.model.UserAccount;
+import com.pascoe.healthyeaterapi.model.Meal;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MealsRepository extends CrudRepository<UserAccount, Integer> {}
+public interface MealsRepository extends CrudRepository<Meal, Integer> {
+  Optional<List<Meal>> findAllByUserId(Integer id);
+}
