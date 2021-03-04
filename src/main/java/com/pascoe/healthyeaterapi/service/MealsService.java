208688fe81885a47a7e540913793fd58b9ccaf14
jpassgo <jeffpascoe9@gmail.com>
2021-03-04 17:44:14 -0600

    Adding getMeals to MealsService.

--- src/main/java/com/pascoe/healthyeaterapi/service/MealsService.java
+++ src/main/java/com/pascoe/healthyeaterapi/service/MealsService.java
@@ -2,20 +2,21 @@ package com.pascoe.healthyeaterapi.service;
 
 import com.pascoe.healthyeaterapi.model.Meal;
 import com.pascoe.healthyeaterapi.repository.MealsRepository;
+import java.util.List;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
+@AllArgsConstructor
 public class MealsService {
 
-    private MealsRepository mealsRepository;
+  private MealsRepository mealsRepository;
 
-    public Meal reportMeal(Meal meal) {
-        return mealsRepository.save(meal);
-    }
+  public Meal reportMeal(Meal meal) {
+    return mealsRepository.save(meal);
+  }
 
-    public List<Meal> getMeals(Integer id) {
-        return mealsRepository.findAllByUserId(id).orElseGet(List::of);
-    }
+  public List<Meal> getMeals(Integer id) {
+    return mealsRepository.findAllByUserId(id).orElseGet(List::of);
+  }
 }
