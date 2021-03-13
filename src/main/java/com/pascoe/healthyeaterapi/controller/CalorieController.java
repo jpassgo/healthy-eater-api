74421d96fb75e2bb98853a5d201c193406be159d
jpassgo <jeffpascoe9@gmail.com>
2021-03-12 18:10:28 -0600

    Creating a controller that will be used to figure out what the calorie count is for a particular food.

---
+++ src/main/java/com/pascoe/healthyeaterapi/controller/CalorieController.java
@@ -0,0 +1,13 @@
+package com.pascoe.healthyeaterapi.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/calories")
+public class CalorieController {
+
+
+}
