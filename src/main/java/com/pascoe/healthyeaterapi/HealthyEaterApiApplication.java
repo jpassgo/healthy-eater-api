b79f386e01687b42bb5bbcf2b50a0303386198af
jpassgo <jeffpascoe9@gmail.com>
2021-03-14 10:19:52 -0500

    Enabling feign clients.

--- src/main/java/com/pascoe/healthyeaterapi/HealthyEaterApiApplication.java
+++ src/main/java/com/pascoe/healthyeaterapi/HealthyEaterApiApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication(
     exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 public class HealthyEaterApiApplication {
