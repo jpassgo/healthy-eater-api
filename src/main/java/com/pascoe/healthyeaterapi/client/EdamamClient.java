0ff1252339806d99c8ccf4eb524b861e09594b36
jpassgo <jeffpascoe9@gmail.com>
2021-03-15 20:54:47 -0500

    Creating a client for calling the edamam API in order to get information about a particular item of food.

---
+++ src/main/java/com/pascoe/healthyeaterapi/client/EdamamClient.java
@@ -0,0 +1,16 @@
+package com.pascoe.healthyeaterapi.client;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(url="${feign.client.edamam-api.url}/", name = "edamam-api")
+public interface EdamamClient {
+
+    @RequestMapping(method = RequestMethod.GET)
+    JsonNode getFoodDetails(@RequestParam(value="app_id") String app_id,
+                            @RequestParam(value="app_key") String app_key,
+                            @RequestParam(value="ingr") String ingr);
+}
