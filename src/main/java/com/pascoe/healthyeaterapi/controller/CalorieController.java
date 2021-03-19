b1e9c2a08d465ed8be9ae6f4ea60a2c151ae4825
jpassgo <jeffpascoe9@gmail.com>
2021-03-18 21:03:20 -0500

    Updating controller.

--- src/main/java/com/pascoe/healthyeaterapi/controller/CalorieController.java
+++ src/main/java/com/pascoe/healthyeaterapi/controller/CalorieController.java
@@ -1,6 +1,6 @@
 package com.pascoe.healthyeaterapi.controller;
 
-import com.pascoe.healthyeaterapi.client.EdamamClient;
+import com.pascoe.healthyeaterapi.service.EdamamService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,24 +8,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/calories")
 public class CalorieController {
 
-    EdamamClient edamamClient;
+    EdamamService edamamService;
+
 
     @GetMapping
     public ResponseEntity getCaloriesForFood(@RequestParam(value="food") String food) {
-         return ResponseEntity.of(Optional.of(edamamClient.getFoodDetails(
-                 "ebd367b9",
-                 "8ab028daa0f4b8b8e6c4ddbf32c28757", food)
-                 .get("parsed")
-                 .get(0)
-                 .get("food")
-                 .get("nutrients")
-                 .get("ENERC_KCAL")));
+         return ResponseEntity.of(edamamService.getCalories(food));
     }
 }
