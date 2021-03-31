a077a4b7dbc5522ae3a23be3ffe842aab4490ee5
jpassgo <jeffpascoe9@gmail.com>
2021-03-30 19:53:33 -0500

    Adding method to get the authentication token.
    Adding method to validate the provided authentication token.

--- src/main/java/com/pascoe/healthyeaterapi/authentication/TokenProvider.java
+++ src/main/java/com/pascoe/healthyeaterapi/authentication/TokenProvider.java
@@ -1,20 +1,27 @@
 package com.pascoe.healthyeaterapi.authentication;
 
 import io.github.jhipster.config.JHipsterProperties;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 import java.util.stream.Collectors;
 
 
+@Slf4j
 @Component
 public class TokenProvider {
 
@@ -34,6 +41,7 @@ public class TokenProvider {
         this.secretKey = jHipsterProperties.getSecurity().getAuthentication().getJwt().getSecret();
         this.tokenValidityInMilliseconds = 1000 * jHipsterProperties.getSecurity()
                 .getAuthentication().getJwt().getTokenValidityInSeconds();
+        this.key = getSigningKey();
     }
 
     public String createToken(Authentication authentication) {
@@ -44,9 +52,48 @@ public class TokenProvider {
         return Jwts.builder()
                 .setSubject(authentication.getName())
                 .claim(AUTHORITIES_KEY, authorities)
-                .signWith(getSigningKey())
+                .signWith(key)
                 .setExpiration(getTokenValidityDuration())
                 .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)
+                .getBody();
+
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+
+        User principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException e) {
+            log.info("Invalid JWT signature.");
+            log.trace("Invalid JWT signature trace: {}", e);
+        } catch (MalformedJwtException e) {
+            log.info("Invalid JWT token.");
+            log.trace("Invalid JWT token trace: {}", e);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT token.");
+            log.trace("Expired JWT token trace: {}", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT token.");
+            log.trace("Unsupported JWT token trace: {}", e);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT token compact of handler are invalid.");
+            log.trace("JWT token compact of handler are invalid trace: {}", e);
+        }
+        return false;
     }
 
     private Date getTokenValidityDuration() {
