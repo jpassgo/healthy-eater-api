241f1fb02e84b2cc351f06c728a79174b9365d21
jpassgo <jeffpascoe9@gmail.com>
2021-04-01 19:48:45 -0500

    Creating a method for extracting the Bearer auth token and passing that in for validation.

--- src/main/java/com/pascoe/healthyeaterapi/filter/AuthTokenFilter.java
+++ src/main/java/com/pascoe/healthyeaterapi/filter/AuthTokenFilter.java
@@ -5,21 +5,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.pascoe.healthyeaterapi.authentication.TokenProvider;
+import io.micrometer.core.instrument.util.StringUtils;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+import static io.micrometer.core.instrument.util.StringUtils.isNotBlank;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+
 @Order(1)
 @Component
 public class AuthTokenFilter implements Filter {
+
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER_TOKEN = "Bearer ";
 
     TokenProvider tokenProvider;
     
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
-        if (tokenProvider.validateToken(httpRequest.getHeader("Authorization"))) {
+        if (tokenProvider.validateToken(extractToken(httpRequest))) {
             chain.doFilter(request, response);
         } else {
             HttpServletResponse httpResponse = (HttpServletResponse) response;
@@ -27,5 +34,14 @@ public class AuthTokenFilter implements Filter {
 
             chain.doFilter(request, httpResponse);
         }
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+
+        if(isNotBlank(bearerToken) && bearerToken.startsWith(BEARER_TOKEN)) {
+            return bearerToken.substring(7);
+        }
+        return EMPTY;
     }
 }
