892c324f358234b6e3bc5f79b935ef0bbeb53c16
jpassgo <jeffpascoe9@gmail.com>
2021-04-02 20:37:28 -0500

    spotless

--- src/main/java/com/pascoe/healthyeaterapi/authentication/TokenProvider.java
+++ src/main/java/com/pascoe/healthyeaterapi/authentication/TokenProvider.java
@@ -5,6 +5,12 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SignatureException;
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -13,97 +19,93 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import java.security.Key;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Date;
-import java.util.stream.Collectors;
-
-
 @Slf4j
 @Component
 public class TokenProvider {
 
-    private static final String AUTHORITIES_KEY = "auth";
-    private String secretKey;
-    private Key key;
-    private long tokenValidityInMilliseconds;
-//    private long tokenValidityInMillisecondsForRememberMe;
-    private final JHipsterProperties jHipsterProperties;
+  private static final String AUTHORITIES_KEY = "auth";
+  private String secretKey;
+  private Key key;
+  private long tokenValidityInMilliseconds;
+  //    private long tokenValidityInMillisecondsForRememberMe;
+  private final JHipsterProperties jHipsterProperties;
 
-    public TokenProvider(JHipsterProperties jHipsterProperties) {
-        this.jHipsterProperties = jHipsterProperties;
+  public TokenProvider(JHipsterProperties jHipsterProperties) {
+    this.jHipsterProperties = jHipsterProperties;
+  }
+
+  @PostConstruct
+  public void init() {
+    this.secretKey = jHipsterProperties.getSecurity().getAuthentication().getJwt().getSecret();
+    this.tokenValidityInMilliseconds =
+        1000
+            * jHipsterProperties
+                .getSecurity()
+                .getAuthentication()
+                .getJwt()
+                .getTokenValidityInSeconds();
+    this.key = getSigningKey();
+  }
+
+  public String createToken(Authentication authentication) {
+    String authorities =
+        authentication.getAuthorities().stream()
+            .map(GrantedAuthority::getAuthority)
+            .collect(Collectors.joining(","));
+
+    return Jwts.builder()
+        .setSubject(authentication.getName())
+        .claim(AUTHORITIES_KEY, authorities)
+        .signWith(key)
+        .setExpiration(getTokenValidityDuration())
+        .compact();
+  }
+
+  public Authentication getAuthentication(String token) {
+    Claims claims = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+
+    Collection<? extends GrantedAuthority> authorities =
+        Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+            .map(SimpleGrantedAuthority::new)
+            .collect(Collectors.toList());
+
+    User principal = new User(claims.getSubject(), "", authorities);
+
+    return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+  }
+
+  public boolean validateToken(String authToken) {
+    try {
+      //
+      // DefaultJwtParserBuilder.setSigningKey(getSigningKey()).build().parseClaimsJws(authToken);
+      Jwts.parser().setSigningKey(getSigningKey()).parseClaimsJws(authToken);
+      return true;
+    } catch (SignatureException e) {
+      log.info("Invalid JWT signature.");
+      log.trace("Invalid JWT signature trace: {}", e);
+    } catch (MalformedJwtException e) {
+      log.info("Invalid JWT token.");
+      log.trace("Invalid JWT token trace: {}", e);
+    } catch (ExpiredJwtException e) {
+      log.info("Expired JWT token.");
+      log.trace("Expired JWT token trace: {}", e);
+    } catch (UnsupportedJwtException e) {
+      log.info("Unsupported JWT token.");
+      log.trace("Unsupported JWT token trace: {}", e);
+    } catch (IllegalArgumentException e) {
+      log.info("JWT token compact of handler are invalid.");
+      log.trace("JWT token compact of handler are invalid trace: {}", e);
     }
+    return false;
+  }
 
-    @PostConstruct
-    public void init() {
-        this.secretKey = jHipsterProperties.getSecurity().getAuthentication().getJwt().getSecret();
-        this.tokenValidityInMilliseconds = 1000 * jHipsterProperties.getSecurity()
-                .getAuthentication().getJwt().getTokenValidityInSeconds();
-        this.key = getSigningKey();
-    }
+  private Date getTokenValidityDuration() {
+    long now = (new Date()).getTime();
+    return new Date(now + this.tokenValidityInMilliseconds);
+  }
 
-    public String createToken(Authentication authentication) {
-        String authorities = authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(","));
-
-        return Jwts.builder()
-                .setSubject(authentication.getName())
-                .claim(AUTHORITIES_KEY, authorities)
-                .signWith(key)
-                .setExpiration(getTokenValidityDuration())
-                .compact();
-    }
-
-    public Authentication getAuthentication(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(key)
-                .parseClaimsJws(token)
-                .getBody();
-
-        Collection<? extends GrantedAuthority> authorities =
-                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
-
-        User principal = new User(claims.getSubject(), "", authorities);
-
-        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
-    }
-
-    public boolean validateToken(String authToken) {
-        try {
-            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(authToken);
-            return true;
-        } catch (SignatureException e) {
-            log.info("Invalid JWT signature.");
-            log.trace("Invalid JWT signature trace: {}", e);
-        } catch (MalformedJwtException e) {
-            log.info("Invalid JWT token.");
-            log.trace("Invalid JWT token trace: {}", e);
-        } catch (ExpiredJwtException e) {
-            log.info("Expired JWT token.");
-            log.trace("Expired JWT token trace: {}", e);
-        } catch (UnsupportedJwtException e) {
-            log.info("Unsupported JWT token.");
-            log.trace("Unsupported JWT token trace: {}", e);
-        } catch (IllegalArgumentException e) {
-            log.info("JWT token compact of handler are invalid.");
-            log.trace("JWT token compact of handler are invalid trace: {}", e);
-        }
-        return false;
-    }
-
-    private Date getTokenValidityDuration() {
-        long now = (new Date()).getTime();
-        return new Date(now + this.tokenValidityInMilliseconds);
-    }
-
-    private Key getSigningKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(this.secretKey);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
-
+  private Key getSigningKey() {
+    byte[] keyBytes = Decoders.BASE64.decode(this.secretKey);
+    return Keys.hmacShaKeyFor(keyBytes);
+  }
 }
--- src/main/java/com/pascoe/healthyeaterapi/filter/AuthTokenFilter.java
+++ src/main/java/com/pascoe/healthyeaterapi/filter/AuthTokenFilter.java
@@ -1,47 +1,45 @@
 package com.pascoe.healthyeaterapi.filter;
 
+import static io.micrometer.core.instrument.util.StringUtils.isNotBlank;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+
+import com.pascoe.healthyeaterapi.authentication.TokenProvider;
+import java.io.IOException;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.pascoe.healthyeaterapi.authentication.TokenProvider;
-import io.micrometer.core.instrument.util.StringUtils;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-
-import static io.micrometer.core.instrument.util.StringUtils.isNotBlank;
-import static org.apache.commons.lang3.StringUtils.EMPTY;
 
 @Order(1)
 @Component
 public class AuthTokenFilter implements Filter {
 
-    private static final String AUTHORIZATION_HEADER = "Authorization";
-    private static final String BEARER_TOKEN = "Bearer ";
+  private static final String AUTHORIZATION_HEADER = "Authorization";
+  private static final String BEARER_TOKEN = "Bearer ";
 
-    TokenProvider tokenProvider;
-    
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        if (tokenProvider.validateToken(extractToken(httpRequest))) {
-            chain.doFilter(request, response);
-        } else {
-            HttpServletResponse httpResponse = (HttpServletResponse) response;
-            httpResponse.setStatus(401);
+  TokenProvider tokenProvider;
 
-            chain.doFilter(request, httpResponse);
-        }
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+    HttpServletRequest httpRequest = (HttpServletRequest) request;
+    if (tokenProvider.validateToken(extractToken(httpRequest))) {
+      chain.doFilter(request, response);
+    } else {
+      HttpServletResponse httpResponse = (HttpServletResponse) response;
+      httpResponse.setStatus(401);
+
+      chain.doFilter(request, httpResponse);
     }
+  }
 
-    private String extractToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+  private String extractToken(HttpServletRequest request) {
+    String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
 
-        if(isNotBlank(bearerToken) && bearerToken.startsWith(BEARER_TOKEN)) {
-            return bearerToken.substring(7);
-        }
-        return EMPTY;
+    if (isNotBlank(bearerToken) && bearerToken.startsWith(BEARER_TOKEN)) {
+      return bearerToken.substring(7);
     }
+    return EMPTY;
+  }
 }
