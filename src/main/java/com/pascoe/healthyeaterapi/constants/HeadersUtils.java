3535e82034bd60ef0862ccdee146052f37546e18
jpassgo <jeffpascoe9@gmail.com>
2021-04-04 12:36:04 -0500

    Adding utils for headers with constants.

---
+++ src/main/java/com/pascoe/healthyeaterapi/constants/HeadersUtils.java
@@ -0,0 +1,6 @@
+package com.pascoe.healthyeaterapi.constants;
+
+public class HeadersUtils {
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER = "Bearer ";
+}
