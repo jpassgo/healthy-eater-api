023e02bdc7b177d39abf02901ed6177951c93cc8
jpassgo <jeffpascoe9@gmail.com>
2021-04-05 20:12:27 -0500

    Removing unneeded annotations.

--- src/main/java/com/pascoe/healthyeaterapi/config/WebSecurityConfig.java
+++ src/main/java/com/pascoe/healthyeaterapi/config/WebSecurityConfig.java
@@ -1,14 +1,21 @@
 package com.pascoe.healthyeaterapi.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
@@ -36,5 +43,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .build();
 
     return new InMemoryUserDetailsManager(user);
+  }
+
+  @Override
+  @Bean
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
   }
 }
