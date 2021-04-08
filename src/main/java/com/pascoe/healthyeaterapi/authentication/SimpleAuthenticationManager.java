3a1ef98736d305ecdce3d84dc49296c5750e2638
jpassgo <jeffpascoe9@gmail.com>
2021-04-07 20:40:46 -0500

    Adding a basic implementation of AuthenticationManager.

---
+++ src/main/java/com/pascoe/healthyeaterapi/authentication/SimpleAuthenticationManager.java
@@ -0,0 +1,34 @@
+package com.pascoe.healthyeaterapi.authentication;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Service
+public class SimpleAuthenticationManager implements AuthenticationManager {
+
+    static final List<GrantedAuthority> AUTHORITIES = new ArrayList<GrantedAuthority>();
+
+    static {
+        AUTHORITIES.add(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    public Authentication authenticate(Authentication auth) throws AuthenticationException {
+        if (auth.getName().equals(auth.getCredentials())) {
+            return new UsernamePasswordAuthenticationToken(auth.getName(),
+                    auth.getCredentials(), AUTHORITIES);
+        }
+        throw new BadCredentialsException("Bad Credentials");
+    }
+
+}
