8a6662b053d933dd46309ccef7ca20a1533748b4
jpassgo <jeffpascoe9@gmail.com>
2021-04-08 20:40:27 -0500

    SpotlessApply Formatting.

--- src/main/java/com/pascoe/healthyeaterapi/authentication/SimpleAuthenticationManager.java
+++ src/main/java/com/pascoe/healthyeaterapi/authentication/SimpleAuthenticationManager.java
@@ -1,5 +1,7 @@
 package com.pascoe.healthyeaterapi.authentication;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -10,25 +12,21 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Component
 @Service
 public class SimpleAuthenticationManager implements AuthenticationManager {
 
-    static final List<GrantedAuthority> AUTHORITIES = new ArrayList<GrantedAuthority>();
+  static final List<GrantedAuthority> AUTHORITIES = new ArrayList<GrantedAuthority>();
 
-    static {
-        AUTHORITIES.add(new SimpleGrantedAuthority("ROLE_USER"));
+  static {
+    AUTHORITIES.add(new SimpleGrantedAuthority("ROLE_USER"));
+  }
+
+  public Authentication authenticate(Authentication auth) throws AuthenticationException {
+    if (auth.getName().equals(auth.getCredentials())) {
+      return new UsernamePasswordAuthenticationToken(
+          auth.getName(), auth.getCredentials(), AUTHORITIES);
     }
-
-    public Authentication authenticate(Authentication auth) throws AuthenticationException {
-        if (auth.getName().equals(auth.getCredentials())) {
-            return new UsernamePasswordAuthenticationToken(auth.getName(),
-                    auth.getCredentials(), AUTHORITIES);
-        }
-        throw new BadCredentialsException("Bad Credentials");
-    }
-
+    throw new BadCredentialsException("Bad Credentials");
+  }
 }
