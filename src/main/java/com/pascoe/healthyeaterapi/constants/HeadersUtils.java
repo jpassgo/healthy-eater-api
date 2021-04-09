95c49727e2e80bbdb1e2804b1411dbf6a760b17d
jpassgo <jeffpascoe9@gmail.com>
2021-04-08 20:39:58 -0500

    SpotlessApply formatting.

--- src/main/java/com/pascoe/healthyeaterapi/constants/HeadersUtils.java
+++ src/main/java/com/pascoe/healthyeaterapi/constants/HeadersUtils.java
@@ -1,6 +1,6 @@
 package com.pascoe.healthyeaterapi.constants;
 
 public class HeadersUtils {
-    public static final String AUTHORIZATION_HEADER = "Authorization";
-    public static final String BEARER = "Bearer ";
+  public static final String AUTHORIZATION_HEADER = "Authorization";
+  public static final String BEARER = "Bearer ";
 }
