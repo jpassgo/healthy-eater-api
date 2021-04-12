f08dc0a6c88cb734a9b7aa1ef95eef072081c5c4
jpassgo <jeffpascoe9@gmail.com>
2021-04-11 20:21:29 -0500

    Allow us to search for an account by user credentials.

--- src/main/java/com/pascoe/healthyeaterapi/repository/UserAccountRepository.java
+++ src/main/java/com/pascoe/healthyeaterapi/repository/UserAccountRepository.java
@@ -1,6 +1,12 @@
 package com.pascoe.healthyeaterapi.repository;
 
 import com.pascoe.healthyeaterapi.model.UserAccount;
+import com.pascoe.healthyeaterapi.model.UserCredentials;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserAccountRepository extends CrudRepository<UserAccount, Integer> {}
+import java.util.Optional;
+
+public interface UserAccountRepository extends CrudRepository<UserAccount, Integer> {
+
+    public Optional<UserAccount> findByUserCredentialsUserName(String userName);
+}
