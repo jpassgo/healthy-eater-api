8c0fe57af12eeccf67703d04c8efd541e790e27e
jpassgo <jeffpascoe9@gmail.com>
2021-04-12 19:31:06 -0500

    Adding logic to validate that the provided user credentials actually exist in the db and that they are valid. If they are we return an auth token.

--- src/main/java/com/pascoe/healthyeaterapi/controller/AuthenticationController.java
+++ src/main/java/com/pascoe/healthyeaterapi/controller/AuthenticationController.java
@@ -3,19 +3,24 @@ package com.pascoe.healthyeaterapi.controller;
 import com.pascoe.healthyeaterapi.authentication.AuthToken;
 import com.pascoe.healthyeaterapi.authentication.SimpleAuthenticationManager;
 import com.pascoe.healthyeaterapi.authentication.TokenProvider;
-import com.pascoe.healthyeaterapi.model.LoginDetails;
+import com.pascoe.healthyeaterapi.model.UserAccount;
+import com.pascoe.healthyeaterapi.model.UserCredentials;
+import com.pascoe.healthyeaterapi.repository.UserAccountRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.security.SecureRandom;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
@@ -24,19 +29,36 @@ public class AuthenticationController {
 
   private final TokenProvider tokenProvider;
   private final SimpleAuthenticationManager authenticationManager;
+  private final UserAccountRepository userAccountRepository;
 
 
   @PostMapping("/authentication")
   public ResponseEntity authenticate(
-      @RequestBody LoginDetails loginDetails, HttpServletResponse response) {
+          @RequestBody UserCredentials userCredentials, HttpServletResponse response) {
+    Optional<UserAccount> optionalUserAccount = userAccountRepository.findByUserCredentialsUserName(userCredentials.getUsername());
+
+    if(optionalUserAccount.isPresent()) {
+      if(doesPasswordMatch(userCredentials, optionalUserAccount)) {
+        String jwt = generateAuthToken(userCredentials);
+        return new ResponseEntity(new AuthToken(jwt), HttpStatus.OK);
+      }
+    }
+
+    return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+  }
+
+  private String generateAuthToken(@RequestBody UserCredentials userCredentials) {
     UsernamePasswordAuthenticationToken authenticationToken =
-        new UsernamePasswordAuthenticationToken(
-            loginDetails.getUsername(), loginDetails.getPassword());
+            new UsernamePasswordAuthenticationToken(
+                    userCredentials.getUsername(), userCredentials.getPassword());
 
     Authentication authentication = authenticationManager.authenticate(authenticationToken);
     SecurityContextHolder.getContext().setAuthentication(authentication);
-    String jwt = tokenProvider.createToken(authentication);
+    return tokenProvider.createToken(authentication);
+  }
 
-    return new ResponseEntity(new AuthToken(jwt), HttpStatus.OK);
+  private boolean doesPasswordMatch(@RequestBody UserCredentials userCredentials, Optional<UserAccount> optionalUserAccount) {
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10, new SecureRandom());
+    return encoder.matches(userCredentials.getPassword(), optionalUserAccount.get().getUserCredentials().getPassword());
   }
 }
