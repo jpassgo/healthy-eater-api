247bf165d9437e3039f76d48ba459fdef5120055
jpassgo <jeffpascoe9@gmail.com>
2021-06-09 20:03:08 -0500

    Using AuthenticationUtils.

--- src/main/java/com/pascoe/healthyeaterapi/controller/AuthenticationController.java
+++ src/main/java/com/pascoe/healthyeaterapi/controller/AuthenticationController.java
@@ -1,21 +1,17 @@
 package com.pascoe.healthyeaterapi.controller;
 
 import com.pascoe.healthyeaterapi.authentication.AuthToken;
-import com.pascoe.healthyeaterapi.authentication.SimpleAuthenticationManager;
-import com.pascoe.healthyeaterapi.authentication.TokenProvider;
 import com.pascoe.healthyeaterapi.model.UserAccount;
 import com.pascoe.healthyeaterapi.model.UserCredentials;
 import com.pascoe.healthyeaterapi.repository.UserAccountRepository;
-import java.security.SecureRandom;
+
 import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
+
+import com.pascoe.healthyeaterapi.service.AuthenticationUtils;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
@@ -24,9 +20,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping
 public class AuthenticationController {
 
-  private final TokenProvider tokenProvider;
-  private final SimpleAuthenticationManager authenticationManager;
   private final UserAccountRepository userAccountRepository;
+  private final AuthenticationUtils authenticationUtils;
 
   @PostMapping("/authentication")
   public ResponseEntity authenticate(
@@ -35,8 +30,8 @@ public class AuthenticationController {
         userAccountRepository.findByUserCredentialsUserName(userCredentials.getUsername());
 
     if (optionalUserAccount.isPresent()) {
-      if (doesPasswordMatch(userCredentials, optionalUserAccount)) {
-        String jwt = generateAuthToken(userCredentials);
+      if (authenticationUtils.doesPasswordMatch(userCredentials, optionalUserAccount)) {
+        String jwt = authenticationUtils.generateAuthToken(userCredentials);
         return new ResponseEntity(new AuthToken(jwt), HttpStatus.OK);
       }
     }
@@ -44,21 +39,5 @@ public class AuthenticationController {
     return new ResponseEntity(HttpStatus.UNAUTHORIZED);
   }
 
-  private String generateAuthToken(@RequestBody UserCredentials userCredentials) {
-    UsernamePasswordAuthenticationToken authenticationToken =
-        new UsernamePasswordAuthenticationToken(
-            userCredentials.getUsername(), userCredentials.getPassword());
 
-    Authentication authentication = authenticationManager.authenticate(authenticationToken);
-    SecurityContextHolder.getContext().setAuthentication(authentication);
-    return tokenProvider.createToken(authentication);
-  }
-
-  private boolean doesPasswordMatch(
-      @RequestBody UserCredentials userCredentials, Optional<UserAccount> optionalUserAccount) {
-    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10, new SecureRandom());
-    return encoder.matches(
-        userCredentials.getPassword(),
-        optionalUserAccount.get().getUserCredentials().getPassword());
-  }
 }
