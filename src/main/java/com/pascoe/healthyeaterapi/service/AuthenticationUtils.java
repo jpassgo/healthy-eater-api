3887071ac4fdefb4fc906ebd973c3fcf8a453b67
jpassgo <jeffpascoe9@gmail.com>
2021-06-12 18:50:48 -0500

    spotless

--- src/main/java/com/pascoe/healthyeaterapi/service/AuthenticationUtils.java
+++ src/main/java/com/pascoe/healthyeaterapi/service/AuthenticationUtils.java
@@ -4,6 +4,8 @@ import com.pascoe.healthyeaterapi.authentication.SimpleAuthenticationManager;
 import com.pascoe.healthyeaterapi.authentication.TokenProvider;
 import com.pascoe.healthyeaterapi.model.UserAccount;
 import com.pascoe.healthyeaterapi.model.UserCredentials;
+import java.security.SecureRandom;
+import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -12,40 +14,37 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.security.SecureRandom;
-import java.util.Optional;
-
 @Service
 @AllArgsConstructor
 public class AuthenticationUtils {
 
-    private final TokenProvider tokenProvider;
-    private final SimpleAuthenticationManager authenticationManager;
+  private final TokenProvider tokenProvider;
+  private final SimpleAuthenticationManager authenticationManager;
 
-    public String generateAuthToken(@RequestBody UserCredentials userCredentials) {
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(
-                        userCredentials.getUsername(), userCredentials.getPassword());
+  public String generateAuthToken(@RequestBody UserCredentials userCredentials) {
+    UsernamePasswordAuthenticationToken authenticationToken =
+        new UsernamePasswordAuthenticationToken(
+            userCredentials.getUsername(), userCredentials.getPassword());
 
-        Authentication authentication = authenticationManager.authenticate(authenticationToken);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        return tokenProvider.createToken(authentication);
-    }
+    Authentication authentication = authenticationManager.authenticate(authenticationToken);
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+    return tokenProvider.createToken(authentication);
+  }
 
-    public String generateAuthToken() {
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken("admin", "pass");
+  public String generateAuthToken() {
+    UsernamePasswordAuthenticationToken authenticationToken =
+        new UsernamePasswordAuthenticationToken("admin", "pass");
 
-        Authentication authentication = authenticationManager.authenticate(authenticationToken);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        return tokenProvider.createToken(authentication);
-    }
+    Authentication authentication = authenticationManager.authenticate(authenticationToken);
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+    return tokenProvider.createToken(authentication);
+  }
 
-    public boolean doesPasswordMatch(
-            @RequestBody UserCredentials userCredentials, Optional<UserAccount> optionalUserAccount) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10, new SecureRandom());
-        return encoder.matches(
-                userCredentials.getPassword(),
-                optionalUserAccount.get().getUserCredentials().getPassword());
-    }
+  public boolean doesPasswordMatch(
+      @RequestBody UserCredentials userCredentials, Optional<UserAccount> optionalUserAccount) {
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10, new SecureRandom());
+    return encoder.matches(
+        userCredentials.getPassword(),
+        optionalUserAccount.get().getUserCredentials().getPassword());
+  }
 }
