f2bf5a29dfac26fccd5c39b6abafbd08dfe722c4
jpassgo <jeffpascoe9@gmail.com>
2021-06-12 18:54:35 -0500

    Adding methods to reports meals and get lists of meals given a username.

--- src/main/java/com/pascoe/healthyeaterapi/controller/MealsController.java
+++ src/main/java/com/pascoe/healthyeaterapi/controller/MealsController.java
@@ -1,6 +1,8 @@
 package com.pascoe.healthyeaterapi.controller;
 
 import com.pascoe.healthyeaterapi.model.Meal;
+import com.pascoe.healthyeaterapi.model.UserCredentials;
+import com.pascoe.healthyeaterapi.service.AccountsService;
 import com.pascoe.healthyeaterapi.service.MealsService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,10 +15,32 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/meals")
 public class MealsController {
 
+  private AccountsService accountsService;
   private MealsService mealsService;
 
   @PostMapping("/{id}")
   public ResponseEntity reportMeals(@PathVariable Integer id, @RequestBody Meal meal) {
+    return reportMeal(meal);
+  }
+
+  @PostMapping("/{id}")
+  public ResponseEntity reportMeals(@RequestBody UserCredentials userCredentials, @RequestBody Meal meal) {
+    meal.setId(accountsService.findAccount(userCredentials).get().getId());
+    return reportMeal(meal);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity getMeals(@PathVariable Integer id) {
+    return retrieveMeals(id);
+  }
+
+  @PostMapping("/{id}")
+  public ResponseEntity getMeals(@RequestBody UserCredentials userCredentials) {
+    Integer id = accountsService.findAccount(userCredentials).get().getId();
+    return retrieveMeals(id);
+  }
+
+  private ResponseEntity reportMeal(@RequestBody Meal meal) {
     try {
       mealsService.reportMeal(meal);
       return new ResponseEntity(HttpStatus.CREATED);
@@ -25,11 +49,10 @@ public class MealsController {
     }
   }
 
-  @GetMapping("/{id}")
-  public ResponseEntity getMeals(@PathVariable Integer id) {
+  private ResponseEntity retrieveMeals(@PathVariable Integer id) {
     return mealsService
-        .getMeals(id)
-        .map(meals -> new ResponseEntity(meals, HttpStatus.OK))
-        .orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
+            .getMeals(id)
+            .map(meals -> new ResponseEntity(meals, HttpStatus.OK))
+            .orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
   }
 }
